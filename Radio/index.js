ae6c4918f78c6d7293f019b64d74f5e66c10929b
Tomasz Szarek <tomasz93szarek@gmail.com>
2018-12-06 21:08:40 +0100

    buffer fix

--- Radio/index.js
+++ Radio/index.js
@@ -10,7 +10,7 @@ Radio.prototype.command = function (cmd, options) {
     let readBufferLength = options && options.readBufferLength
     if (data) {
       writeArray.concat([data])
-      this.spi.transfer(writeArray, function (error, data) {
+      this.spi.transfer(new Buffer(writeArray, function (error, data) {
         if (error) {
           reject(error)
         } else {
@@ -18,7 +18,7 @@ Radio.prototype.command = function (cmd, options) {
         }
       })
     } else {
-      this.spi.transfer(writeArray, readBufferLength, function (error, data) {
+      this.spi.transfer(new Buffer(writeArray), readBufferLength, function (error, data) {
         if (error) {
           reject(error)
         } else {
